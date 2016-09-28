4a14a75b9456b7101275f33b7547afa921e18268
Kevin Costello <kevincostello213@gmail.com>
2016-09-27 17:19:50 -0700

    Initial commit

---
+++ js/MazeScript.js
@@ -0,0 +1,22 @@
+/* Author: Kevin Costello 
+ * Date: 9/27/2016 
+ */
+
+
+function buildMaze() {
+	// Create a maze using Eller's algorithm
+
+}
+
+function solveMaze() {
+	// Solve and show solution (in browser)
+
+}
+
+function start() {
+	buildMaze();
+	solveMaze();
+	console.log("Done!");
+}
+
+start();
